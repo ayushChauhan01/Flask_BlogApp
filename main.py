5d066222ec31827f0b8fceecb0e6a3222b6cbf29
Ayush Chauhan <ac417573@gmail.com>
2022-12-30 00:47:05 +0530

    FrontEnd done mostly

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='templates')
-bootstrap = Bootstrap(app)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template("index.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
-@app.route("/ozil")
-def benzi_or_messi():
-    return "<h1>By Boi Benzi!!!</h1>"
-
-
-@app.route("/user/<name>")
-def user(name):
-    return render_template('user.html', user_name=name)
-
+@app.route('/post')
+def post():
+    return render_template("post.html")
 
 app.run(debug=True)
